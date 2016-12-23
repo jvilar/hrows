e8751f46f0eda148a639d17f96578c15016cb1ef
Juan Miguel Vilar <jvilar@uji.es>
2016-12-23 12:17:22 +0100

    Added testParser.hs

---
+++ src/testParser.hs
@@ -0,0 +1,24 @@
+import System.Console.Readline(addHistory, readline)
+
+import Model.Expression(evaluate, toFormula, toString)
+import Model.Lexer(tokenize)
+import Model.Parser(parse)
+
+main :: IO ()
+main = do
+    putStrLn "Introduce expressions, exit or Ctrl-D to end."
+    loop
+
+loop :: IO()
+loop = do
+    i <- readline "% "
+    case i of
+        Nothing -> return ()
+        Just formula -> do
+            addHistory formula
+            let expression = parse formula
+            putStrLn $ "Tokens: " ++ show (tokenize formula)
+            putStrLn $ "Expresion: " ++ show expression
+            putStrLn $ "As formula: " ++ toFormula expression
+            putStrLn $ "Evaluated: " ++ toString (evaluate [] expression)
+            loop
