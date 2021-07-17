424ccbadf86b86014df7534c0f44af08c783d1b3
Juan Miguel Vilar <jvilar@uji.es>
2021-07-17 18:59:11 +0200

    Added vrows

---
+++ lib/TUI.hs
@@ -0,0 +1,122 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module TUI (
+  startTUI
+) where
+
+import Brick hiding (getName)
+import Brick.Widgets.Border
+import Brick.Widgets.Center
+import Brick.Widgets.List
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import Graphics.Vty.Attributes(defAttr)
+import Graphics.Vty.Input.Events(Event(EvKey), Key(..), Modifier(MCtrl))
+
+import Model.RowStore
+
+data State = State { sRowStore :: RowStore
+                   , sIndex :: Int
+                   , sFieldList :: List Name Text
+                   , sValueList :: List Name Text
+                   }
+
+initialState :: RowStore -> State
+initialState rst = State { sRowStore = rst
+                         , sIndex = 0
+                         , sFieldList = listMoveTo 0 $ fieldList rst
+                         , sValueList = valueList 0 rst
+                         }
+
+fieldList :: RowStore -> List Name Text
+fieldList rst = list FieldList (V.fromList $ fnames rst) 1
+
+valueList :: Int -> RowStore -> List Name Text
+valueList pos rst = list ValueList (V.fromList $ map toString $ row pos rst) 1
+
+
+type EventType = ()
+
+data Name = FieldList | ValueList deriving (Eq, Ord, Show)
+
+draw :: State -> [Widget Name]
+draw s = [
+    joinBorders $ center $
+       borderWithLabel (txt $ title s) $
+       renderFields s
+       <=>
+       hBorder
+       <=>
+       hCenter (txt "C-q: exit")
+    ]
+
+
+tshow :: Int -> Text
+tshow = T.pack . show
+
+
+title :: State -> Text
+title State{..} = T.concat [ getName sRowStore
+                           , " (", tshow $ sIndex + 1, "/"
+                           , tshow $ size sRowStore, ")"
+                           ]
+
+
+renderFields :: State -> Widget Name
+renderFields State {..} = renderList renderName False sFieldList
+                          <+>
+                          vBorder
+                          <+>
+                          renderList renderValue False sValueList
+
+renderName :: Bool -> Text -> Widget Name
+renderName _ = txt
+
+renderValue :: Bool -> Text -> Widget Name
+renderValue _ = txt
+
+
+
+app :: App State EventType Name
+app = App { appDraw = draw
+          , appChooseCursor = showFirstCursor
+          , appHandleEvent = handleEvent
+          , appStartEvent = return
+          , appAttrMap = const myAttrMap
+          }
+
+
+handleEvent :: State -> BrickEvent Name EventType -> EventM Name (Next State)
+handleEvent s (VtyEvent (EvKey (KChar 'q') [MCtrl])) = halt s
+handleEvent s (VtyEvent (EvKey KPageUp [])) = backward s
+handleEvent s (VtyEvent (EvKey KPageDown [])) = forward s
+handleEvent s _ = continue s
+
+backward :: State -> EventM Name (Next State)
+backward s@State {..} = continue $ moveTo (sIndex - 1) s
+
+
+forward :: State -> EventM Name (Next State)
+forward s@State {..} = continue $ moveTo (sIndex + 1) s
+
+
+moveTo :: Int -> State -> State
+moveTo pos s@State{..}
+  | valid = s { sIndex = pos
+              , sValueList = valueList pos sRowStore
+              }
+  | otherwise = s
+    where valid = 0 <= pos && pos < size sRowStore
+
+
+myAttrMap :: AttrMap
+myAttrMap = attrMap defAttr []
+
+
+startTUI :: RowStore -> IO ()
+startTUI rst = do
+  finalState <- defaultMain app (initialState rst)
+  return ()
+
---
+++ src/VRows.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Main where
+
+import Control.Exception(try)
+import Control.Monad(unless, when)
+import Control.Monad.Trans(liftIO)
+import Control.Lens(makeLenses, Getting, (^.), set)
+import Data.Default(Default(..))
+import Data.Maybe(isJust)
+import qualified Data.Text as T
+import System.Directory (doesFileExist)
+import System.Environment(getArgs, getProgName)
+import System.Exit(exitFailure, exitSuccess)
+import System.IO (hPutStrLn, stderr)
+
+import System.Console.JMVOptions
+
+import HRowsException
+import Model.DefaultFileNames
+import Model.RowStore
+import Model.SourceInfo
+import TUI
+
+data Options = Options { _help :: Bool
+                       , _inputFileName :: Maybe FilePath
+                       , _confFileName :: Maybe FilePath
+                       , _inputSeparator :: Char
+                       }
+
+makeLenses ''Options
+
+instance Default Options where
+    def = Options  { _help = False
+                   , _inputFileName = Nothing
+                   , _confFileName = Nothing
+                   , _inputSeparator = ltInputSeparator def
+                   }
+
+defValue :: Show a => Getting a Options a -> String
+defValue field = "Default: " ++ show (def ^. field) ++ "."
+
+options :: [OptDescr (Options -> Options)]
+options = processOptions $ do
+              'h' ~: s "help" ==> NoArg (set help True) ~: s "This help."
+              's' ~: s "separator" ==> ReqArg (set inputSeparator . head) "SEP" ~: s "Separator for input of listatab files. " ++ defValue inputSeparator
+          where s :: String -> String
+                s = id
+
+getOptions :: IO Options
+getOptions = do
+               args <- getArgs
+               let (o, a, e) =  getOpt Permute options args
+               let opt = foldl (flip id) def o
+               when (opt ^. help) $ putStrLn helpMessage >> exitSuccess
+               unless (null e) $ myError $ concat e ++ helpMessage
+               case a of
+                   [] -> myError "No filename given"
+                   [f] -> return $ set inputFileName (Just f) opt
+                   [f, c] -> return $ set inputFileName (Just f) $ set confFileName (Just c) opt
+                   _ -> myError "Too many filenames"
+
+helpMessage :: String
+helpMessage = usageInfo header options
+              where header = "Usage: vrows [Options] <file> [<conf>]"
+
+myError :: String -> IO a
+myError m = do
+              n <- getProgName
+              hPutStrLn stderr $ n ++ " error: " ++ m
+              exitFailure
+
+
+main :: IO ()
+main = do
+  opts <- getOptions
+  let Just fn = opts ^. inputFileName
+  cnf <- if isJust $ opts ^. confFileName
+         then return $ opts ^. confFileName
+         else do
+                let defFn = defaultConfFileName fn
+                exFn <- doesFileExist fn
+                exCnf <- doesFileExist defFn
+                return $ if exFn == exCnf
+                         then Just defFn
+                         else Nothing
+  let pc = PathAndConf fn cnf
+      ltinfo = def { ltInputSeparator = opts ^. inputSeparator }
+      sinfo =  mkSourceInfo Nothing pc ltinfo
+
+  r <- liftIO $ try $ readRowStore sinfo
+  case r of
+      Right (rst, mconf) -> startTUI rst
+      Left (HRowsException mess) -> error $ T.unpack mess
+
+
