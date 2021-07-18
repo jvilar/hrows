c86a81567db3460b987ef281cc44ce60acd67dbe
Juan Miguel Vilar <jvilar@uji.es>
2021-07-18 11:45:18 +0200

    Initial zoom

--- lib/TUI.hs
+++ lib/TUI.hs
@@ -12,7 +12,7 @@ import Brick.Widgets.List
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Vector as V
-import Graphics.Vty.Attributes(defAttr)
+import Graphics.Vty.Attributes(defAttr, reverseVideo, withStyle)
 import Graphics.Vty.Input.Events(Event(EvKey), Key(..), Modifier(MCtrl))
 
 import Model.RowStore
@@ -22,7 +22,12 @@ data State = State { sRowStore :: RowStore
                    , sFieldList :: List Name Text
                    , sFieldWidth :: Int
                    , sValueList :: List Name Text
+                   , sZoom :: Maybe Zoom
                    }
+
+
+type Zoom = (Text, Text)
+
 
 initialState :: RowStore -> State
 initialState rst = State { sRowStore = rst
@@ -30,11 +35,14 @@ initialState rst = State { sRowStore = rst
                          , sFieldList = listMoveTo 0 fl
                          , sFieldWidth = min 40 (V.maximum . V.map T.length $ listElements fl)
                          , sValueList = valueList 0 rst
+                         , sZoom = Nothing
                          }
                     where fl = fieldList rst
 
+
 fieldList :: RowStore -> List Name Text
 fieldList rst = list FieldList (V.fromList $ fnames rst) 1
+
 
 valueList :: Int -> RowStore -> List Name Text
 valueList pos rst = list ValueList (V.fromList $ map toString $ row pos rst) 1
@@ -46,6 +54,7 @@ data Name = FieldList | ValueList deriving (Eq, Ord, Show)
 
 draw :: State -> [Widget Name]
 draw s = [
+    renderZoom s,
     joinBorders $ center $
        borderWithLabel (txt $ title s) $
        renderFields s
@@ -77,11 +86,19 @@ renderFields State {..} = Widget Greedy Fixed $ do
                 , renderList renderValue False sValueList
               ]
 
+
 renderName :: Bool -> Text -> Widget Name
-renderName _ = myTxt
+renderName = renderElement
+
 
 renderValue :: Bool -> Text -> Widget Name
-renderValue _ = myTxt
+renderValue = renderElement
+
+
+renderElement :: Bool -> Text -> Widget Name
+renderElement False = myTxt
+renderElement True = withAttr "selectedElement" . myTxt
+
 
 myTxt :: Text -> Widget n
 myTxt t = Widget Fixed Fixed $ do
@@ -93,6 +110,12 @@ myTxt t = Widget Fixed Fixed $ do
       render $ txt t'
 
 
+renderZoom :: State -> Widget Name
+renderZoom State {..} = case sZoom of
+    Nothing -> txt ""
+    Just (lbl, t) -> borderWithLabel (txt lbl) $ txt t
+
+
 app :: App State EventType Name
 app = App { appDraw = draw
           , appChooseCursor = showFirstCursor
@@ -102,11 +125,18 @@ app = App { appDraw = draw
           }
 
 
+listKeys :: [Key]
+listKeys = [KDown, KUp]
+
+
 handleEvent :: State -> BrickEvent Name EventType -> EventM Name (Next State)
 handleEvent s (VtyEvent (EvKey (KChar 'q') [MCtrl])) = halt s
 handleEvent s (VtyEvent (EvKey KPageUp [])) = backward s
 handleEvent s (VtyEvent (EvKey KPageDown [])) = forward s
+handleEvent s (VtyEvent (EvKey KEnter [])) = zoom s
+handleEvent s (VtyEvent e@(EvKey k [])) | k `elem` listKeys = moveLists s e
 handleEvent s _ = continue s
+
 
 backward :: State -> EventM Name (Next State)
 backward s@State {..} = continue $ moveTo (sIndex - 1) s
@@ -114,6 +144,22 @@ backward s@State {..} = continue $ moveTo (sIndex - 1) s
 
 forward :: State -> EventM Name (Next State)
 forward s@State {..} = continue $ moveTo (sIndex + 1) s
+
+
+zoom :: State -> EventM Name (Next State)
+zoom s@State {..} = do
+  let z = do
+             lbl <- snd <$> listSelectedElement sFieldList
+             t <- snd <$> listSelectedElement sValueList
+             return (lbl, t)
+  continue s { sZoom = z }
+
+
+moveLists :: State -> Event -> EventM Name (Next State)
+moveLists s@State{..} e = do
+    fl <- handleListEvent e sFieldList
+    vl <- handleListEvent e sValueList
+    continue s { sFieldList = fl, sValueList = vl }
 
 
 moveTo :: Int -> State -> State
@@ -126,7 +172,7 @@ moveTo pos s@State{..}
 
 
 myAttrMap :: AttrMap
-myAttrMap = attrMap defAttr []
+myAttrMap = attrMap defAttr [ ("selectedElement", withStyle defAttr reverseVideo) ]
 
 
 startTUI :: RowStore -> IO ()
