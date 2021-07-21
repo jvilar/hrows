1250db962b6de58ed08fc3142bef025f802bc0ec
Juan Miguel Vilar <jvilar@uji.es>
2021-07-21 18:36:26 +0200

    The conf extension is now added instead of replaced

--- lib/Model/DefaultFileNames.hs
+++ lib/Model/DefaultFileNames.hs
@@ -2,11 +2,11 @@ module Model.DefaultFileNames ( defaultConfFileName
                               , defaultBackupFileName
                               ) where
 
-import System.FilePath((-<.>))
+import System.FilePath((<.>))
 
 -- |The default configuration file from a file name.
 defaultConfFileName :: FilePath -> FilePath
-defaultConfFileName = (-<.> "conf")
+defaultConfFileName = (<.> "conf")
 
 -- |The name of the backup file.
 defaultBackupFileName :: FilePath -> FilePath
