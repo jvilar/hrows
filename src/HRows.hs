65e387ce3284993d70f9caf016fb23d6cdbf5781
Juan Miguel Vilar <jvilar@uji.es>
2022-06-24 12:49:26 +0200

    Updated the options of hrows

--- src/HRows.hs
+++ src/HRows.hs
@@ -1,18 +1,15 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module Main where
 
 import Control.Auto.Run(runOnChanM)
 import Control.Concurrent(forkIO, threadDelay)
 import Control.Concurrent.Chan(Chan, newChan, writeChan, writeList2Chan)
-import Control.Lens (makeLenses, (^.), set, Getting)
+import Control.Lens ((^.), set)
 import Control.Monad(forM_, unless, void, when)
 import Data.Default(Default(..))
 import Data.GI.Gtk.Threading(postGUIASync)
 import qualified GI.Gtk as Gtk
-import System.Environment(getArgs, getProgName)
-import System.Exit(exitFailure, exitSuccess)
-import System.IO (hPutStrLn, stderr)
+import System.Environment(getArgs)
+import System.Exit(exitSuccess)
 
 import System.Console.JMVOptions
 
@@ -20,34 +17,12 @@ import GUI.Build
 import GUI.Update
 import Model.SourceInfo
 import Presenter
+import Col
 
-data Options = Options { _help :: Bool
-                       , _inputFileName :: Maybe FilePath
-                       , _confFileName :: Maybe FilePath
-                       , _inputSeparator :: Char
-                       , _outputSeparator :: Char
-                       }
-
-makeLenses ''Options
-
-instance Default Options where
-    def = Options  { _help = False
-                   , _inputFileName = Nothing
-                   , _confFileName = Nothing
-                   , _inputSeparator = ltSeparator def
-                   , _outputSeparator = ltSeparator def
-                   }
-
-defValue :: Show a => Getting a Options a -> String
-defValue field = "Default: " ++ show (def ^. field) ++ "."
+type Options = ColOptions
 
 options :: [OptDescr (Options -> Options)]
-options = processOptions $ do
-              'h' ~: s "help" ==> NoArg (set help True) ~: s "This help."
-              's' ~: s "separator" ==> ReqArg ((\c -> set inputSeparator c . set outputSeparator c) . head) "SEP" ~: s "Separator for input of listatab files. " ++ defValue inputSeparator
-              -- TODO 'S' ~: s "oSeparator" ==> ReqArg (set outputSeparator . head) "SEP" ~: s "Separator for output listatab files. Default: use the one passed to separator."
-          where s :: String -> String
-                s = id
+options = colOptions OnlyInputNoFilterOptions id
 
 getOptions :: IO Options
 getOptions = do
@@ -66,12 +41,6 @@ helpMessage :: String
 helpMessage = usageInfo header options
               where header = "Usage: hrows [Options] <file> [<conf>]"
 
-myError :: String -> IO a
-myError m = do
-              n <- getProgName
-              hPutStrLn stderr $ n ++ " error: " ++ m
-              exitFailure
-
 -- |Thread to write a backup every minute
 backupLoop :: Chan Input -> IO ()
 backupLoop inputChan = do
@@ -84,8 +53,7 @@ main = do
   opts <- getOptions
   let Just fn = opts ^. inputFileName
   pc <- mkPathAndConf fn $ opts ^. confFileName
-  let ltinfo = def { ltSeparator = opts ^. inputSeparator }
-      sinfo =  mkSourceInfo Nothing pc ltinfo
+  let sinfo =  mkSourceInfo Nothing pc $ opts ^. iOptions
 
   inputChan <- newChan
   control <- makeGUI inputChan
