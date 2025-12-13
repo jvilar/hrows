83d96efb0f7db999711102275d4967aded4e92f3
Juan Miguel Vilar <jvilar@uji.es>
2025-12-13 16:05:39 +0100

    shell.nix: added XDG_DATA_DIRS to shellHook

--- shell.nix
+++ shell.nix
@@ -4,8 +4,22 @@ with pkgs;
 
 mkShell {
   buildInputs = [
-    (haskell-language-server.override { supportedGhcVersions = ["966"]; })
+    gtk3
+    gsettings-desktop-schemas
+    haskell-language-server
   ];
+
+  shellHook = ''
+      # Start with GSETTINGS_SCHEMA_PATH
+      local _xdg_data_dirs="$GSETTINGS_SCHEMA_PATH"
+
+      # If XDG_DATA_DIRS is already set, append it
+      if [ -n "$XDG_DATA_DIRS" ]; then
+        _xdg_data_dirs="$_xdg_data_dirs:$XDG_DATA_DIRS"
+      fi
+
+      export XDG_DATA_DIRS="$_xdg_data_dirs"
+  '';
 }
 
 
