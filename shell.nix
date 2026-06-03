925d33ad8d4baf9c6c637042c70838c797bb0343
Juan Miguel Vilar <jvilar@uji.es>
2026-06-03 14:18:48 +0200

    shell.nix: corrected environment variable

--- shell.nix
+++ shell.nix
@@ -17,7 +17,7 @@ mkShell {
     export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS"
     
     # También es vital que GLib sepa dónde buscar los esquemas compilados (gschemas.compiled)
-    export GSETTINGS_SCHEMA_PATH="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas"
+    export GSETTINGS_SCHEMAS_PATH="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas"
   '';
 }
 
